6d5e80fc98fb8596041e2f3f281f5e9e8f3ca10d
urosArsenijevic <urosars@gmail.com>
2022-12-11 15:01:15 +0100

    Initial commit

---
+++ js/coinbin.js
@@ -0,0 +1,213 @@
+$(document).ready(function() {
+
+	/* open wallet code */
+
+	var explorer_addr = "https://coinb.in/addr/"
+
+	var email;
+	var pass;
+	var s;
+
+
+	var wallet_timer = false;
+
+	$("#openBtn").click(function(){
+
+					for(i=0;i<=50;i++){
+						s = Crypto.SHA256(s);
+					}
+
+					coinjs.compressed = true;
+					var keys = coinjs.newKeys(s);
+					var address = keys.address;
+					var wif = keys.wif;
+					var pubkey = keys.pubkey;
+					var privkeyaes = CryptoJS.AES.encrypt(keys.wif, pass);
+
+					$("#walletKeys .walletSegWitRS").addClass("hidden");
+					if($("#walletSegwit").is(":checked")){
+						if($("#walletSegwitBech32").is(":checked")){
+							var sw = coinjs.bech32Address(pubkey);
+							address = sw.address;
+						} else {
+
+							var sw = coinjs.segwitAddress(pubkey);
+							address = sw.address;
+						}
+
+						$("#walletKeys .walletSegWitRS").removeClass("hidden");
+						$("#walletKeys .walletSegWitRS input:text").val(sw.redeemscript);
+					}
+
+					$("#walletAddress").html(address);
+					$("#walletHistory").attr('href',explorer_addr+address);
+
+					$("#walletQrCode").html("");
+					var qrcode = new QRCode("walletQrCode");
+					qrcode.makeCode("bitcoin:"+address);
+
+					$("#walletKeys .privkey").val(wif);
+					$("#walletKeys .pubkey").val(pubkey);
+					$("#walletKeys .privkeyaes").val(privkeyaes);
+
+					$("#confirmation").hide();
+					$("#openWallet").removeClass("hidden").show();
+
+					walletBalance();
+	});
+
+	$("#openBlinkingSDK").click(function(){
+
+		var email = $("#openEmail").val().toLowerCase();
+		if(email.match(/[\s\w\d]+@[\s\w\d]+/g)){
+			if($("#openPass").val().length>=10){
+				if($("#openPass").val()==$("#openPassConfirm").val()){
+					email = $("#openEmail").val().toLowerCase();
+					pass = $("#openPass").val();
+					s = email;
+					s += '|'+pass+'|';
+					s += s.length+'|!@'+((pass.length*7)+email.length)*7;
+					var regchars = (pass.match(/[a-z]+/g)) ? pass.match(/[a-z]+/g).length : 1;
+					var regupchars = (pass.match(/[A-Z]+/g)) ? pass.match(/[A-Z]+/g).length : 1;
+					var regnums = (pass.match(/[0-9]+/g)) ? pass.match(/[0-9]+/g).length : 1;
+					s += ((regnums+regchars)+regupchars)*pass.length+'3571';
+					s += (s+''+s);
+
+					$("#openLogin").hide();
+					$("#callBlinkingSDK").removeClass("hidden").show();
+
+				} else {
+					$("#openLoginStatus").html("Your passwords do not match!").removeClass("hidden").fadeOut().fadeIn();
+				}
+			} else {
+				$("#openLoginStatus").html("Your password must be at least 10 chars long").removeClass("hidden").fadeOut().fadeIn();
+			}
+		} else {
+			$("#openLoginStatus").html("Your email address doesn't appear to be valid").removeClass("hidden").fadeOut().fadeIn();
+		}
+
+		$("#openLoginStatus").prepend('<span class="glyphicon glyphicon-exclamation-sign"></span> ');
+
+	});
+
+	$("#openCalendar").click(function(){
+
+		$("#callBlinkingSDK").hide();
+		$("#calendar").removeClass("hidden").show();
+		$("#calendarOpenSuccess").html("Congratulations! You have successfully passed the KYC check and now you can schedule a video meet with one of our Customer Support agents.").removeClass("hidden").fadeOut().fadeIn();
+	});
+
+	$("#alertKYC").click(function(){
+
+		$("#callBlinkingSDK").hide();
+		$("#calendar").removeClass("hidden").show();
+	});
+
+	$("#callVideoIdentification").click(function(){
+		$("#calendar").hide();
+		$("#videoIdentification").removeClass("hidden").show().day(day);
+	});
+
+	$("#callConfirmation").click(function(){
+
+		$("#videoIdentification").hide();
+		$("#confirmation").removeClass("hidden").show();
+	});
+
+	$("#createWallet").click(function(){
+
+		$("#confirmation").hide();
+		$("#openWallet").removeClass("hidden").show();
+	});
+
+	$("#walletLogout").click(function(){
+		$("#openEmail").val("");
+		$("#openPass").val("");
+		$("#openPassConfirm").val("");
+
+		$("#openLogin").show();
+		$("#openWallet").addClass("hidden").show();
+
+		$("#walletAddress").html("");
+		$("#walletHistory").attr('href',explorer_addr);
+
+		$("#walletQrCode").html("");
+		var qrcode = new QRCode("walletQrCode");
+		qrcode.makeCode("bitcoin:");
+
+		$("#walletKeys .privkey").val("");
+		$("#walletKeys .pubkey").val("");
+
+		$("#openLoginStatus").html("").hide();
+	});
+
+	$("#walletShowKeys").click(function(){
+		$(".walletOptions").removeClass("hidden").addClass("hidden");
+		$("#walletKeys").removeClass("hidden");
+	});
+
+	$("#walletSendBtn").click(function(){
+
+		$("#walletSendFailTransaction").addClass('hidden');
+		$("#walletSendStatus").addClass("hidden").html("");
+
+		var thisbtn = $(this);
+		var txfee = $("#txFee");
+		var devamount = $("#developerDonation");
+
+		if((!isNaN(devamount.val())) && devamount.val()>=0){
+			$(devamount).parent().removeClass('has-error');
+		} else {
+			$(devamount).parent().addClass('has-error')
+		}
+
+		if((!isNaN(txfee.val())) && txfee.val()>=0){
+			$(txfee).parent().removeClass('has-error');
+		} else {
+			$(txfee).parent().addClass('has-error');
+		}
+
+		var total = (devamount.val()*1) + (txfee.val()*1);
+
+		$.each($("#walletSpendTo .output"), function(i,o){
+			var amount = $('.amount',o);
+			var address = $('.addressTo',o);
+
+			total += amount.val()*1;
+
+			if((!isNaN($(amount).val())) && $(amount).val()>0){
+				$(amount).parent().removeClass('has-error');
+			} else {
+				$(amount).parent().addClass('has-error');
+			}
+
+			if(coinjs.addressDecode($(address).val())){
+				$(address).parent().removeClass('has-error');
+			} else {
+				$(address).parent().addClass('has-error');
+			}
+		});
+
+		total = total.toFixed(8);
+
+		if($("#walletSpend .has-error").length==0){
+			var balance = ($("#walletBalance").html()).replace(/[^0-9\.]+/g,'')*1;
+			if(total<=balance){
+				$("#walletSendConfirmStatus").addClass("hidden").removeClass('alert-success').removeClass('alert-danger').html("");
+				$("#spendAmount").html(total);
+				$("#modalWalletConfirm").modal("show");
+				$("#walletConfirmSend").attr('disabled',false);
+			} else {
+				$("#walletSendStatus").removeClass("hidden").html("You are trying to spend "+total+' but have a balance of '+balance);
+			}
+		} else {
+			$("#walletSpend .has-error").fadeOut().fadeIn();
+			$("#walletSendStatus").removeClass("hidden").html('<span class="glyphicon glyphicon-exclamation-sign"></span> One or more input has an error');
+		}
+	});
+
+	$("#walletShowSpend").click(function(){
+		$(".walletOptions").removeClass("hidden").addClass("hidden");
+		$("#walletSpend").removeClass("hidden");
+	});
+});
